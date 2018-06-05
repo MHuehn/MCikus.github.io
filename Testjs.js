199b9237896b361fc660f58654b2a47c1fed2840
MCikus <cikus.marcel@gmail.com>
2018-06-05 11:27:25 +0200

    Create Testjs.js

---
+++ Testjs.js
@@ -0,0 +1,11 @@
+// Create an object:
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+// Display some data from the object:
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
